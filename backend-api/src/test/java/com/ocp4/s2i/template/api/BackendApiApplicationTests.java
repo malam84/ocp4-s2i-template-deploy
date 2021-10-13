ed88a8757eba054ad5c2f34758d763d8e9e18e6d
Maqsood Alam <malam84@gmail.com>
2021-10-13 10:31:01 +0800

    Deployment on openshift using template

---
+++ backend-api/src/test/java/com/ocp4/s2i/template/api/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp4.s2i.template.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
